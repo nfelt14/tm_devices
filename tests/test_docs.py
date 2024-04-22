273ccf0a176bb57163a9866955c3f644ab766205
Nicholas Felt <nicholas.felt@tektronix.com>
2024-04-22 11:38:40 -0700

    test: Got doc tests working.

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -2,9 +2,9 @@
 
 import os
 import shlex
-import shutil
 import subprocess
 import sys
+import time
 
 from importlib.util import find_spec
 from typing import Generator
@@ -13,19 +13,37 @@ import pytest
 
 from conftest import PROJECT_ROOT_DIR
 
+OUTPUT_DIR = f"site_{sys.version_info.major}{sys.version_info.minor}"
+
+
+@pytest.fixture(name="docs_server")
+def fixture_docs_server() -> Generator[str, None, None]:
+    """Serve the documentation site."""
+    port = int(f"8{sys.version_info.major}{sys.version_info.minor}")
+    cmd = f"{sys.executable} -m http.server {port} --directory={OUTPUT_DIR}"
+    with subprocess.Popen(
+        cmd,
+        shell=True,  # noqa: S602
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ) as server_process:
+        time.sleep(1)  # wait for server to start
+
+        yield f"http://127.0.0.1:{port}/"
+
+        server_process.terminate()  # stop the server
+        try:
+            server_process.wait(timeout=10)  # Wait up to 10 seconds for the server to terminate
+        except subprocess.TimeoutExpired:
+            server_process.kill()  # Force kill if it doesn't terminate in time
+
 
 @pytest.fixture(scope="module", autouse=True)
-def _use_docs_directory() -> Generator[None, None, None]:  # pyright: ignore [reportUnusedFunction]
-    """Use a custom docs directory for these tests."""
-    os.chdir(PROJECT_ROOT_DIR)
-    build_dir = f".docs_{sys.version_info.major}{sys.version_info.minor}"
+def _docs_tests_setup() -> Generator[None, None, None]:  # pyright: ignore [reportUnusedFunction]
+    """Setup for docs tests.."""
     starting_directory = os.getcwd()
-    shutil.rmtree(build_dir, ignore_errors=True)
-    shutil.copytree("docs", build_dir, ignore=shutil.ignore_patterns("_*"))
-    shutil.copytree("docs/_static", build_dir + "/_static")
-    shutil.copytree("docs/_templates", build_dir + "/_templates")
     try:
-        os.chdir(build_dir)
+        os.chdir(PROJECT_ROOT_DIR)
         yield
     finally:
         os.chdir(starting_directory)
@@ -33,26 +51,21 @@ def _use_docs_directory() -> Generator[None, None, None]:  # pyright: ignore [re
 
 @pytest.mark.docs
 @pytest.mark.slow
-@pytest.mark.skipif(find_spec("sphinx") is None, reason="The sphinx module is not installed.")
+@pytest.mark.skipif(find_spec("mkdocs") is None, reason="The mkdocs package is not installed.")
 class TestDocs:  # pylint: disable=no-self-use
     """A collection of documentation tests."""
 
     @pytest.mark.order(1)
     def test_docs_html(self) -> None:
         """Test creating html documentation."""
-        subprocess.check_call(shlex.split("make html"))  # noqa: S603
+        subprocess.check_call(shlex.split(f"mkdocs build --site-dir={OUTPUT_DIR}"))  # noqa: S603
 
     @pytest.mark.order(2)
-    def test_docs_linkcheck(self) -> None:
+    @pytest.mark.depends(on=["test_docs_html"])
+    def test_docs_linkcheck(self, docs_server: str) -> None:
         """Run the linkcheck test for the documentation."""
-        subprocess.check_call(shlex.split("make linkcheck"))  # noqa: S603
-
-    @pytest.mark.order(3)
-    def test_docs_coverage(self) -> None:
-        """Run the coverage test for the documentation."""
-        subprocess.check_call(shlex.split("make coverage"))  # noqa: S603
-
-    @pytest.mark.order(4)
-    def test_docs_doctest(self) -> None:
-        """Run the doctest test for the documentation."""
-        subprocess.check_call(shlex.split("make doctest"))  # noqa: S603
+        subprocess.check_call(
+            shlex.split(  # noqa: S603
+                f"linkchecker --no-status --config=docs/.linkchecker.ini {docs_server}"
+            )
+        )
