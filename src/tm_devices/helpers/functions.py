e9afa0ed2c75c62e3e81ab9ded82fc5af985e5d9
Nicholas Felt <nicholas.felt@tektronix.com>
2024-05-29 10:15:05 -0700

    ci: debugging

--- src/tm_devices/helpers/functions.py
+++ src/tm_devices/helpers/functions.py
@@ -298,8 +298,8 @@ def check_visa_connection(
 
 
 def create_visa_connection(
-    visa_library: str,
     device_config_entry: DeviceConfigEntry,
+    visa_library: str,
     *,
     retry_connection: bool = False,
 ) -> MessageBasedResource:
